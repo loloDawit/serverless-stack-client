61247270f2de9e463dcc778666e4ba92b75b426d
loloDawit <dawitabera64@gmail.com>
2019-11-01 10:19:22 -0700

    updated login btn

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
-import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import "./Login.css";
 import { Auth } from "aws-amplify";
+import LoaderButton from "../components/LoaderButton";
 
 export default function Login(props) {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
 
     function validateForm() {
         return email.length > 0 && password.length > 0;
@@ -13,12 +15,16 @@ export default function Login(props) {
 
     async function handleSubmit(event) {
         event.preventDefault();
+        setIsLoading(true);
 
         try {
             await Auth.signIn(email, password);
-            alert("Logged in");
+            props.userHasAuthenticated(true);
+            props.history.push("/");
+            //alert("Logged in");
         } catch (e) {
             alert(e.message);
+            setIsLoading(false);
         }
     }
 
@@ -42,9 +48,14 @@ export default function Login(props) {
                         type="password"
                     />
                 </FormGroup>
-                <Button block bsSize="large" disabled={!validateForm()} type="submit">
-                    Login
-        </Button>
+                <LoaderButton
+                    block
+                    type="submit"
+                    bsSize="large"
+                    isLoading={isLoading}
+                    disabled={!validateForm()}
+                >Login
+                </LoaderButton>
             </form>
         </div>
     );
