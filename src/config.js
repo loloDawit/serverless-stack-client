044dc66fe9c87940799f9790ad4cfdaadc6b69c7
loloDawit <dawitabera64@gmail.com>
2019-11-01 09:00:39 -0700

    Setting up front end app using react

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-1",
+        BUCKET: "note-application-uploads"
+    },
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: "https://pgzuhbmr76.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_9qM9wjmwU",
+        APP_CLIENT_ID: "6dcqhmcgc7ds1o9tg7dtu0fkmb",
+        IDENTITY_POOL_ID: "us-east-1:796b95ac-53a9-4c2b-83bd-d8f2c6f781db"
+    }
+};
