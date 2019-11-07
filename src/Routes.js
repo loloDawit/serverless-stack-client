94f56203472d2b23ad8e4a4f28ae960d1f38fba6
loloDawit <dawitabera64@gmail.com>
2019-11-07 13:21:00 -0800

    temp fix

--- src/Routes.js
+++ src/Routes.js
@@ -1,50 +1,26 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import asyncComponent from "./components/AsyncComponent";
+import Home from "./containers/Home";
+import NotFound from "./containers/NotFound";
+import Login from "./containers/Login";
 import AppliedRoute from "./components/AppliedRoute";
+import Signup from "./containers/Signup";
+import NewNote from "./containers/NewNote";
+import Notes from "./containers/Notes";
+import Settings from "./containers/Settings";
 import AuthenticatedRoute from "./components/AuthenticatedRoute";
 import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
-
-const AsyncHome = asyncComponent(() => import("./containers/Home"));
-const AsyncLogin = asyncComponent(() => import("./containers/Login"));
-const AsyncNotes = asyncComponent(() => import("./containers/Notes"));
-const AsyncSignup = asyncComponent(() => import("./containers/Signup"));
-const AsyncNewNote = asyncComponent(() => import("./containers/NewNote"));
-const AsyncNotFound = asyncComponent(() => import("./containers/NotFound"));
-
-export default ({ childProps }) =>
-  <Switch>
-    <AppliedRoute
-      path="/"
-      exact
-      component={AsyncHome}
-      props={childProps}
-    />
-    <UnauthenticatedRoute
-      path="/login"
-      exact
-      component={AsyncLogin}
-      props={childProps}
-    />
-    <UnauthenticatedRoute
-      path="/signup"
-      exact
-      component={AsyncSignup}
-      props={childProps}
-    />
-    <AuthenticatedRoute
-      path="/notes/new"
-      exact
-      component={AsyncNewNote}
-      props={childProps}
-    />
-    <AuthenticatedRoute
-      path="/notes/:id"
-      exact
-      component={AsyncNotes}
-      props={childProps}
-    />
-    {/* Finally, catch all unmatched routes */}
-    <Route component={AsyncNotFound} />
-  </Switch>
-;
+export default function Routes({ appProps }) {
+  return (
+    <Switch>
+      <AppliedRoute path="/" exact component={Home} appProps={appProps} />
+      <UnauthenticatedRoute path="/login" exact component={Login} appProps={appProps} />
+      <UnauthenticatedRoute path="/signup" exact component={Signup} appProps={appProps} />
+      <AuthenticatedRoute path="/settings" exact component={Settings} appProps={appProps} />
+      <AuthenticatedRoute path="/notes/new" exact component={NewNote} appProps={appProps} />
+      <AuthenticatedRoute path="/notes/:id" exact component={Notes} appProps={appProps} />
+      { /* Finally, catch all unmatched routes */}
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
